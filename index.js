4c5db5d1bf11b37e111e6b95f02f602e8b2b4de0
Offirmo <offirmo.net@gmail.com>
2015-11-23 16:18:52 +0100

    repluced npm traverse by a simplified version

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // node-only file
 
 var easyconf = require('./lib/easyconf');
-
 var dotenv = require('dotenv');
-dotenv.config({silent: true});
 
 module.exports = easyconf;
 module.exports.dotenv = dotenv;
